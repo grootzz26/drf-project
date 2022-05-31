8cef89b738fdab0083f33be7d0587eaca22ba0f9
Sargunaraj <sargunaraj@FLENDZZ-PC032.vpn.flendzz.com>
2022-05-31 10:56:55 +0530

    first commit

--- app1/serializers.py
+++ app1/serializers.py
@@ -12,18 +12,21 @@ class AddedBySerializer(serializers.ModelSerializer):
 class ProductSerializer(serializers.ModelSerializer):
 
     user_data = AddedBySerializer(many=True, source='user_list')
-    total_price = serializers.SerializerMethodField()
+    # total_price = serializers.SerializerMethodField()
 
     class Meta:
         model = product
         fields = ['name','type', 'price', 'tax','total_price', 'user_data']
 
-    def get_total_price(self, obj):
-        return obj.price + (obj.price * obj.tax // 100)
+    # def get_total_price(self, obj):
+    #     return obj.price + (obj.price * obj.tax // 100)
 
     def create(self, validated_data):
+        price = validated_data.get('price')
+        tax = validated_data.get('tax')
+        value = price + (price * tax//100)
         users = validated_data.pop('user_list')  # {"user": "guna", "items": 4 }
-        products = product.objects.create(**validated_data)
+        products = product.objects.create(total_price=value,**validated_data)
 
         for i in users:
             Added_by.objects.create(items=products, **i)
